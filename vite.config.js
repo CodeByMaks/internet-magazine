333a6ec5c25af32f1455ca1ffcf05f9fa5c4758c
CodeByMaks <maks667741@gmail.com>
2025-05-23 12:02:26 +0500

    m

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     port: 3000,
-    open: true,
+    strictPort: true,
+    host: true,
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
